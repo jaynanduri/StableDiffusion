0444ca115510b97bacfff8ab2e718b079a2e466b
jaynanduri <jayantha.nanduri@gmail.com>
2023-12-09 23:02:50 -0500

    API for stable diffusion pipeline

--- StableDiffusion/urls.py
+++ StableDiffusion/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    re_path('^', include('diffusion.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- diffusion/views.py
+++ diffusion/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import base64
+import json
+from io import BytesIO
+import base64
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .LatentDiffusion.model import StableDiffusion
 
 # Create your views here.
+diffuser = StableDiffusion()
+
+
+@csrf_exempt
+def post_generated_image(request):
+    if request.method == "POST":
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        content = body['prompt']
+        image = diffuser.generate(content)
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        return JsonResponse({"image": img_str})
+    else:
+        return JsonResponse({"error": "This api only handles post requests"})
